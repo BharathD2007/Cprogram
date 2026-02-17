ad18882fc6ec50e8bf22331cb9dc0246f6763efb
BharathD2007 <itsbharath00@gmail.com>
2026-02-17 11:59:31 +0530

    Add batting team win likelihood prediction program
    This program calculates the run rate and predicts the likelihood of the batting team winning based on the total score and overs played.

---
+++ Conditional statement/Determines the likelihood of the batting team winning the match.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    float total_score, total_overs;
+    float run_rate, percentage;
+    
+    printf("Enter total score: ");
+    scanf("%f", &total_score);
+
+    printf("Enter total overs played: ");
+    scanf("%f", &total_overs);
+
+    if (total_overs <= 0) {
+        printf("Overs must be greater than 0.\n");
+    } 
+    else {
+        run_rate = total_score / total_overs;
+        percentage = (run_rate / 6) * 100;
+
+        printf("Run Rate: %.2f\n", run_rate);
+        printf("Percentage: %.2f%%\n", percentage);
+
+        if (percentage < 50) {
+            printf("Prediction: Opponent team can win.\n");
+        }
+        else if (percentage == 50) {
+            printf("Prediction: Both teams have an equal chance of winning.\n");
+        }
+        else {
+            printf("Prediction: Batting team can win.\n");
+        }
+    }
+}
